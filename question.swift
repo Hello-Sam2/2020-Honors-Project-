f240ebec2a7f74539d1dacd07386586803c44072
Sam Beacher <23Samuelb@lrei.org>
2020-03-02 14:44:12 -0500

    Question Game all files

---
+++ question.swift
@@ -0,0 +1,37 @@
+//
+//  gamestates.swift
+//  QuestionGame
+//
+//  Created by Sam Beacher on 2/28/20.
+//  Copyright © 2020 Sam Beacher. All rights reserved.
+//
+
+import Foundation
+
+struct Everything {
+    let answer : ([String]) -> Bool
+}
+//  var a = "Arabic" = false
+//  var b = "English" = true
+//  var c = "Spanish" = false
+//  var d = "French" = false
+
+
+
+
+
+
+let question1 = "What language is spoken by most people in Africa?"
+
+func submitAnswer1(answer: Everything) -> Bool {
+    question1
+    if ([b]) {
+        return true
+    }
+    else if [(a,c,d)] {
+        return false
+    }
+    
+
+}
+
